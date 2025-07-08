834f4e4469ade080ff55ad68c79067b1719158a4
esteban@eviniegra.software <esteban@eviniegra.software>
2025-07-08 21:46:41 +0200

    rollback

--- __manifest__.py
+++ __manifest__.py
@@ -1,3 +1,4 @@
+# beneficiarias/__manifest__.py
 {
     'name': 'Beneficiarias',
     'version': '1.0',
@@ -6,22 +7,14 @@
     'author': 'Esteban Viniegra Pérez Olagaray | Pridecta',
     'website': 'https://pridecta.com',
     'license': 'LGPL-3',
-    'depends': [
-        'base',
-        'web', 
-    ],
+    'depends': ['base'],
     'data': [
         'security/beneficiarias_security.xml',
-        'security/ir.model.access.csv',
+        'security/ir.model.access.csv', 
         'views/beneficiaria_views.xml',
         'views/beneficiaria_form.xml',
     ],
-    # 'assets': {
-    #     'web.assets_backend': [
-    #         'beneficiarias/static/src/css/beneficiaria_notebook.css',
-    #     ],
-    # },
     'icon': 'beneficiarias/static/description/icon.svg',
     'installable': True,
-    'application': True,
+    'application': True,  
 }
